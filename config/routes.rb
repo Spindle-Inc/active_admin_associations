d576da0f8bef590e0f3388518b287abff12511fb
Justin Clark <jclark@spindle.com>
2015-10-11 23:31:42 -0700

    Apply Fixes to Known Issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 if Rails.application.config.activeadmin_associations.autocomplete_models
   models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
-  ActiveAdminAssociations::Engine.routes.draw do
-    get '/autocomplete/:model', :to => 'autocomplete#index', :model => /(#{models})/,
-      :defaults => { :format => 'json' }
-  end
+  Rails.application.routes.draw do
+     get '/autocomplete/:model', :to => 'autocomplete#index', :model => /(#{models})/,
+       :defaults => { :format => 'json' }
+   end
 end
